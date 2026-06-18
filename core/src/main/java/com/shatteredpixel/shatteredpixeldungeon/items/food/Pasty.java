90f92275dd6755b7caffd38189ecd0e5cdbc8c03
等待响应 <1793418102@qq.com>
2026-06-18 17:45:14 +0800

    【√】给肉桂卷的节日加一周

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Pasty.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Pasty.java
@@ -76,7 +76,7 @@ public class Pasty extends Food {
 						holiday = Holiday.BREAD;
 					break;
 				case Calendar.JUNE:
-					if (calendar.get(Calendar.WEEK_OF_MONTH) <= 2)
+					if (calendar.get(Calendar.WEEK_OF_MONTH) <= 3)
 						holiday = Holiday.BREAD;
 					break;
 			}
